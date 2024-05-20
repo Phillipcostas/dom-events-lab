19797f290073c1c2a27465c95bde25ca2c706a56
PhillipCostas <Phillipcostas@gmail.com>
2024-05-20 09:32:16 -0400

    dom-events-labs-1

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 /*-------------------------------- Variables --------------------------------*/
 let display = document.getElementById("display");
 let currentNumber = 0
-let firstNum, secondNum 
+let firstNum, secondNum
 let currentOperator
-let clear = 0 
+//let clear = 0
 /*------------------------ Cached Element References ------------------------*/
 
 
@@ -18,76 +18,89 @@ let clear = 0
 const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      if ([...event.target.classList].includes('number')) {
-        currentNumber = event.target.innerText
-        displayBar.innerText = currentNumber
-     
-      } else {
-        if (event.target.innerText === '+') {
-          currentOperator = '+'
-           if (firstNum === undefined) {
-            firstNum = currentNumber
-           }  
-        } else if (event.target.innerText === '-') {
-            currentOperator = '-'
-               if (firstNum === undefined) {
-                firstNum = currentNumber
-          }
-      } else if (event.target.innerText === '*') {
-          currentOperator = '*'
-          if (firstNum === undefined) {
-            firstNum = currentNumber
-          } 
-      } else if (event.target.innerText === '/') {
-          currentOperator = '/'
-          if (firstNum === undefined) 
-            firstNum = currentNumber
-  
-      } else if (event.target.innerText === '=') {
-        console.log(currentOperator)
+        if ([...event.target.classList].includes('number')) {
+            currentNumber = event.target.innerText
+            displayBar.innerText = currentNumber
 
-          if (currentOperator === "+")
-            displayBar.innerText = sum (parseInt(currentNumber), parseInt(firstNum))
-              
-          } else if (currentOperator === "-") {
-                displayBar.innerText = minus (parseInt(currentNumber), parseInt(firstNum))    
-          } else if 
-             (currentOperator === '*') {
-              displayBar.innerText = mult (parseInt(currentNumber), parseInt(firstNum))
-            
-          } else if 
-           (currentOperator === '/') {
-              displayBar.innerText = div (parseInt(currentNumber), parseInt(firstNum))
-            }
-          }
-  
-        currentNumber = 0
-        firstNumber = undefined
-        secondNum = undefined
-        currentOperator = undefined
-        })
-      })
+        } else {
 
-      clear()
+            if (event.target.innerText === '+') {
+                console.log(currentNumber)
+                currentOperator = '+'
+                console.log(currentOperator)
+
+                if (firstNum === undefined) {
+                    firstNum = currentNumber
+                }
+
+            } else if (event.target.innerText === '-') {
+                //console.log('hi')
+                currentOperator = '-'
+                if (firstNum === undefined) {
+                    firstNum = currentNumber
+                }
+            } else if (event.target.innerText === '*') {
+                // console.log('hi')
+                currentOperator = '*'
+                if (firstNum === undefined) {
+                    firstNum = currentNumber
+                }
+
+            } else if (event.target.innerText === '/') {
+                // console.log('hi')
+
+                currentOperator = '/'
+                if (firstNum === undefined) {
+                    firstNum = currentNumber
+                }
+            } else if (event.target.innerText === '=') {
+                console.log(currentOperator)
+
+                if (currentOperator === "+") {
+                    displayBar.innerText = sum(parseInt(currentNumber), parseInt(firstNum))
+                } else if (currentOperator === "-") {
+                  displayBar.innerText = minus(parseInt(currentNumber), parseInt(firstNum))
+              } else if (currentOperator === '*') {
+                  displayBar.innerText = multiply(parseInt(currentNumber), parseInt(firstNum))
+  
+              } else if (currentOperator === '/') {
+                  displayBar.innerText = divide(parseInt(currentNumber), parseInt(firstNum))
+              }
+                currentNumber = 0
+                firstNum = undefined
+                secondNum = undefined
+                currentOperator = undefined
+            } 
+        }
+    })
+     }
+)
+
+const clear = () => {
+  if (currentOperator === 'C')  {
+    return 0 
+  }
+}
+
 
 
 /*-------------------------------- Functions --------------------------------*/
 let displayBar = document.querySelector(".display");
-//console.log(displayBar)
 
-const sum = (num1 , num2) => {
-  return num1 + num2
+
+const sum = (num1, num2) => {
+    return num1 + num2
 }
 
- const minus = (num1, num2) => {
-  return num1 - num2 
- }
+const minus = (num1, num2) => {
+    return num1 - num2
+}
 
- const mult = (num1, num2) => {
-  return num1 * num2
- }
+const multiply = (num1, num2) => {
+    return num1 * num2
+}
 
- const div = (num1, num2) => {
-  return num1 / num2
- }
+const divide = (num1, num2) => {
+    return num1 / num2
+}
 
